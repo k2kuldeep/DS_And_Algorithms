14712d3a78d69dfe5e6bc04ca00f6a16f1d3ddd4
pk7856 <pksharma7856@gmail.com>
2019-08-14 09:41:41 +0530

    Selection Sort Algorithm

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,22 +1,21 @@
-//The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order)
-//from unsorted part and putting it at the beginning.
-//time complexity = O(n2)
-//space complexity = O(1)
+/*Selection Sort Implementation.(UNSTABLE)
+Select the minimum element fromn the array and keep it at beginning. Then select smaller element from remaining array and keep it at 2nd position.
+At any time array is divided into two parts : one sorted and other unsorted.
+time complexity = O(n2)
+space complexity = O(1)
+*/
 public class SelectionSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[]= {64,25,12,22,11};
-		
-		sortArray(arr);
-		printArray(arr);
-		
-	}
-
-	private static void sortArray(int[] arr) {
-		// TODO Auto-generated method stub
 		int len = arr.length;
 		
+		for(int i:arr)
+			System.out.print(i+" ");
+		System.out.println();
+		
+		//code
 		for(int i=0;i<len-1;i++){
 			
 			int minIndex = i;
@@ -29,13 +28,8 @@ public class SelectionSort {
 			arr[i] = arr[minIndex];
 			arr[minIndex] = temp;
 		}
-	}
-	
-
-	private static void printArray(int arr[]) {
-		// TODO Auto-generated method stub
+		
 		for(int i:arr)
-			System.out.print(i+"\t");
-	}
-	
+			System.out.print(i+" ");
+	}	
 }
