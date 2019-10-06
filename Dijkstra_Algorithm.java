d8858b3e682b80611548b2782554420d9bc1194f
pk7856 <pksharma7856@gmail.com>
2019-10-06 22:15:07 +0530

    Dijkstra Updated

--- Dijkstra_Algorithm.java
+++ Dijkstra_Algorithm.java
@@ -71,8 +71,8 @@ public class Dijkstra_Algorithm {
  *The code is for undirected graph, same Dijkstra function can be used for directed graphs also.
  *The code finds shortest distances from source to all vertices. If we are interested only in shortest distance from the source to a single target, 
  *we can break the for the loop when the picked minimum distance vertex is equal to target.
- *Time Complexity of the implementation is O(V^2). If the input graph is represented using adjacency list,
- *it can be reduced to O(E log V) with the help of binary heap.
+ *Time Complexity of the implementation is O(V^2). 
+ *If the input graph is represented using adjacency list, it can be reduced to O(E log V) with the help of binary heap.
  *Dijkstra’s algorithm doesn’t work for graphs with negative weight edges. 
  For graphs with negative weight edges, Bellman–Ford algorithm can be used.
  */
